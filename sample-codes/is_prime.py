c31981d25a28c5938ffcc6f4a270430118d1de09
SaurabhD Chavan <saurabhd.10711372@ltimindtree.com>
2024-11-10 14:16:52 +0530

    added the function in prime number code

--- sample-codes/is_prime.py
+++ sample-codes/is_prime.py
@@ -1,14 +1,21 @@
 # This is the code to check if the number is prime or not
 
-number = int(input("Enter the number: "))
-is_prime = True
+# Creating a function
+def is_prime_number():
+    number = int(input("Enter the number: "))
+    is_prime = True
 
-for i in range (2 , number):
-    if number % i == 0:
-        is_prime = False
-        break
+    for i in range (2 , number):
+        if number % i == 0:
+            is_prime = False
+            break
 
-if is_prime :
-    print("The" , number , "is prime number")
-else:
-    print("The", number , "is not a prime number")
+    if is_prime :
+        print("The" , number , "is prime number")
+    else:
+        print("The", number , "is not a prime number")
+
+
+# Using the function
+
+is_prime_number()
